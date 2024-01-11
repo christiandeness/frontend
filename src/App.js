fe0f1025953900b946d31437bc2e955c6061a600
christiandeness <deness.christianbenjamin@gmail.com>
2024-01-11 17:03:20 +0800

    enlooo

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SelectRoom from "./screens/SelectRoom";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RoomScreen from "./screens/RoomScreen";
+import wallpaper from "./wallpaper.jpg";
+import "./index.css";
+
+const appStyle = {
+  position: 'relative',
+  backgroundImage: `url(${wallpaper})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  minHeight: '100vh',
+};
+
+function App() {
+  return (
+    <Router>
+       <div style={appStyle}>
+        <Header />
+        <div className="image-overlay">
+          <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<SelectRoom />} exact />
+            <Route path="/product/:id" element={<RoomScreen />} exact />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+      </div>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
---
+++ src/products.js
@@ -0,0 +1,40 @@
+const products = [
+  {
+    _id: "1",
+    name: "Classic Room",
+    image: "/images/ClassicRoom.jpg",
+    description: "This is a classic room, DUH!?",
+    price: 8000,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Executive Queen",
+    image: "/images/ExecutiveQueenRoom.jpg",
+    description: "LOL mid",
+    price: 8499,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Premier Family Suite",
+    image: "/images/Premier Family Suite.jpg",
+    description: "dont be cheap, get the best of the best",
+    price: 9499,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Presidential Suite",
+    image: "/images/PresidentialSuite.jpg",
+    description: "best of the best iykyk",
+    price: 10000,
+    rating: 5,
+    numReviews: 12,
+  },
+];
+
+export default products;
